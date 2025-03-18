12fc806b57b1a9dd12cb2086ec587802e088160f
mateo927 <mateocolllet07@icloud.com>
2025-03-18 18:44:00 +0100

    18/03/2025bis

--- algos/fourmi/fourmi.py
+++ algos/fourmi/fourmi.py
@@ -1,5 +1,7 @@
 #uv run -m algos.fourmi.fourmi
+from random import randint 
 import random
+from re import S
 type matrice[T] = list[list[T]]
 type chemin = list[int]
 
@@ -19,10 +21,16 @@ def calculer_visibilite(graphe: matrice[float]) -> matrice[float]:
     """Renvoie la matrice de visibilité.
     La visibilité de chaque arête est l'inverse de la distance    """
     lst=[]
-    mat=[]
+    matrice=[]
     for ele in graphe:
-        for e in ele:
-            lst
+        for i in ele:
+            if i==0:
+                lst.append(0)
+            else:
+                lst.append(1/i)
+        matrice.append(lst)
+        lst=[]
+    return matrice
             
             
 
@@ -33,7 +41,43 @@ def get_graphe_exemple() -> matrice[float]:
         [9, 6, 0, 8],
         [10, 4, 8, 0]
     ]
-    
+
+def prochain_sommet(pheromones: matrice[float], visibilite: matrice[float],sommet_courant: int,inexplore: list[int],alpha: float, beta: float) -> int:
+    """Renvoit le prochain sommet au hasard en fonction de l attractivité des chemins.    Notre fourmi est quand même plus intelligente qu'une fourmi classique, elle ne va pas aux    endroits déjà visités.        
+    >>> random.seed(54)    
+    >>> g = get_graphe_exemple()    
+    >>> n = len(g)    "
+    >>> prochain_sommet(init_pheromones(n), calculer_visibilite(g), 0, list(range(n)), 1, 2)    1 """
+    proba=[]
+    somme=0
+    for i in inexplore:
+        tmp=pheromones[sommet_courant][i]**alpha*visibilite[sommet_courant][i]**beta
+        somme+=tmp
+        proba.append(tmp)
+    proba=[i/somme for i in proba]
+    return random.choices(inexplore,weights=proba)
+
+def parcours_fourmi( graphe: matrice[float],pheromones: matrice[float],visibilite: matrice[float],alpha: float, beta: float) -> tuple[chemin, float]:
+    """    Simule le parcours d'une seule fourmi choisissant au hasard un sommet de départ inexploré.    
+    Renvoie le chemin ainsi que la longueur de ce chemin.    
+    >>> random.seed(54)    
+    >>> g = get_graphe_exemple()    
+    >>> n = len(g)    
+    >>> parcours_fourmi(g, init_pheromones(n), calculer_visibilite(g), 1, 2)    ([1, 0, 2, 3, 1], 23)    """
+    tt=0
+    lst=[]
+    inexplore=[i for i in range(len(graphe))]
+    sommet_courant=randint(0,len(graphe)-1)
+    lst.append(sommet_courant)
+    inexplore.remove(sommet_courant)
+    sommet_suivant=prochain_sommet(pheromones,visibilite,sommet_courant,inexplore,alpha,beta)
+    while sommet_suivant!=[]:
+        sommet_courant=sommet_suivant[0]
+        lst.append(sommet_courant)
+        inexplore.remove(sommet_courant)
+        sommet_suivant=prochain_sommet(pheromones,visibilite,sommet_courant,inexplore,alpha,beta)
+        tt+=graphe[lst[-2]][lst[-1]]
+    return lst,tt
 if __name__ == "__main__":
     import doctest
     doctest.testmod()
